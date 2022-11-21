b7f3af33f67815ae4614e9dffee9be9edf767818
Jack Raney <j-raney.1@onu.edu>
2022-11-21 16:25:30 -0500

    wip other functions before calculate
    TODO: convert everything into just text and then parse that text when calculating

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -10,6 +10,9 @@ var outputText;
 
 var nextIsNegative = false;
 
+var isDecimal = false;
+var decimalNumbers;
+
 window.onload = function()
 {
     var digitList = document.querySelectorAll(".digit");
@@ -29,16 +32,31 @@ window.onload = function()
             inputOperator(e.target.value);
         });
     });
+    document.getElementById("invert").addEventListener("click", invertNumber);
+    document.getElementById("decimal").addEventListener("click", convertToDecimal);
 
     document.getElementById("delete").addEventListener("click", deleteLastInput);
     document.getElementById("clear").addEventListener("click", clear);
+    document.getElementById("equals").addEventListener("click", calculate);
 }
 
 function addToInputDisplay(value)
 {
-    if (!inputText.length || !isNaN(inputText[inputText.length - 1]) && !isNaN(value))
+    if (!isNaN(value))
     {
-        inputText = inputText.concat(value);
+        if (value < 0)
+        {
+            value = "(" + value + ")"
+        }
+
+        if (!inputText.length || !isNaN(inputText[inputText.length - 1]))
+        {
+            inputText = inputText.concat(value);
+        }
+        else
+        {
+            inputText = inputText.concat(" ", value)
+        }
     }
     else
     {
@@ -50,13 +68,36 @@ function addToInputDisplay(value)
 
 function inputNumber(digit)
 {
-    if (inputStack.length && !isNaN(inputStack[inputStack.length - 1]))
+    if (isDecimal)
     {
-        inputStack[inputStack.length - 1] = inputStack[inputStack.length - 1] * 10 + (inputStack[inputStack.length - 1] < 0 ? digit * -1 : digit * 1);
+        if (decimalNumbers == null)
+        {
+            decimalNumbers = digit;
+
+            document.getElementById("decimal").disabled = true;
+        }
+
+        else
+        {
+            decimalNumbers.concat(digit)
+        }
     }
     else
     {
-        inputStack.push(nextIsNegative ? -digit : digit);
+        if (inputStack.length && !isNaN(inputStack[inputStack.length - 1]))
+        {
+            inputStack[inputStack.length - 1] = inputStack[inputStack.length - 1] * 10 + (inputStack[inputStack.length - 1] < 0 ? digit * -1 : digit * 1);
+        }
+        else
+        {
+            if (nextIsNegative)
+            {
+                digit = -digit;
+                nextIsNegative = false;
+            }
+    
+            inputStack.push(digit);
+        }
     }
 
     addToInputDisplay(digit);
@@ -70,6 +111,12 @@ function inputOperator(operator)
     }
     else
     {
+        if (isDecimal)
+        {
+            isDecimal = false;
+            decimalNumbers = "." + decimalNumbers;
+            inputStack[inputStack.length -1] += decimalNumbers;
+        }
         inputStack.push(operator);
     }
 
@@ -78,16 +125,36 @@ function inputOperator(operator)
 
 function deleteLastInput()
 {
-    if (isNaN(inputStack[inputStack.length - 1]))
+    if (!isNaN(inputStack[inputStack.length - 1]) && Math.abs(inputStack[inputStack.length - 1]) < 10)
     {
-        inputStack.pop();
+        let deletedNumber = inputStack.pop();
+
+        if (deletedNumber < 0)
+        {
+            inputText = inputText.slice(0, inputText.lastIndexOf("(")).trim();
+        }
+        else
+        {
+            inputText = inputText.slice(0, -1).trim();
+        }
     }
     else
     {
-        inputStack[inputStack.length - 1] /= 10;
+        if (isNaN(inputStack[inputStack.length - 1]))
+        {
+            inputStack.pop();
+        }
+        else
+        {
+            if (inputStack[inputStack.length - 1] % 1 !== 0)
+            {
+                
+            }
+            inputStack[inputStack.length - 1] /= 10;
+        }
+        
+        inputText = inputText.slice(0, -1).trim();
     }
-
-    inputText = inputText.slice(0, -1);
     document.getElementById("input").innerHTML = inputText;
 }
 
@@ -95,5 +162,60 @@ function clear()
 {
     inputStack = [];
     inputText = "";
+    outputText = "";
     document.getElementById("input").innerHTML = inputText;
+    document.getElementById("output").innerHTML = outputText;
+}
+
+function invertNumber()
+{
+    if (!inputStack.length || isNaN(inputStack[inputStack.length - 1]))
+    {
+        nextIsNegative = !nextIsNegative;
+    }
+    else
+    {
+        inputStack[inputStack.length - 1] = -inputStack[inputStack.length - 1];
+        invertDisplayedNumber();
+    }
+}
+
+function convertToDecimal()
+{
+    if (isDecimal)
+    {
+        isDecimal = false;
+
+        inputText = inputText.slice(0, inputText.length - 1);
+    }
+
+    isDecimal = true;
+
+    addToInputDisplay("0.");
+}
+
+// this is gonna need a huge rework when I incorporate actual parentheses
+function invertDisplayedNumber()
+{
+    if (inputText[inputText.length - 1] === ")")
+    {
+        let numberStartingIndex = inputText.lastIndexOf("(");
+        let numberToInvert = inputText.substring(inputText.lastIndexOf("("));
+        let invertedNumber = numberToInvert.substring(2, numberToInvert.length - 1);
+        inputText = inputText.substring(0, numberStartingIndex) + invertedNumber;
+    }
+    else
+    {
+        let lastNumber = inputText.match(/\d+\.?\d*$/);
+        let numberStartingIndex = inputText.lastIndexOf(lastNumber[0]);
+        let invertedNumber = "(" + inputStack[inputStack.length - 1] + ")";
+        inputText = inputText.substring(0, numberStartingIndex) + invertedNumber;
+    }
+
+    document.getElementById("input").innerHTML = inputText;
+}
+
+function calculate()
+{
+
 }
